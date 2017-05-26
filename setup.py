ac88e66ca9fce40b69ae4ae2b1c06a8ca7ff0f6c
Michel Albert <michel@albert.lu>
2017-05-26 16:25:59 +0200

    Initial import

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+VERSION = open('pow/version.txt').read()
+setup(
+    name="pow",
+    version=VERSION.strip(),
+    packages=find_packages(),
+    install_requires=[
+        'config-resolver >= 4.2, <5.0',
+        'flask',
+        'flask-sqlalchemy',
+        'gouge >= 1.1, <2.0',
+        'psycopg2',
+        'sqlalchemy',
+    ],
+    extras_require={
+        'dev': [
+            'alembic',
+        ],
+        'test': [
+            'pytest',
+            'pytest-cache',
+            'pytest-catchlog',
+        ]
+    },
+    include_package_data=True,
+    author="Michel Albert",
+    author_email="michel@albert.lu",
+    description="Tracker for PowWow 2017",
+    license="BSD",
+    url="http://exhuma.github.com/pow",
+)
