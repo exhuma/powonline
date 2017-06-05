e8d4d7211430b98d866e269a4a48903fbef03354
Michel Albert <michel@albert.lu>
2017-06-05 12:33:13 +0200

    Colorize CLI output on dev server.

--- autoapp.py
+++ autoapp.py
@@ -1,3 +1,7 @@
 from powonline.web import make_app
+from powonline.util import colorize_werkzeug
+from gouge.colourcli import Simple
+Simple.basicConfig(level=0)
+colorize_werkzeug()
 APP = make_app()
 APP.run(debug=True, host='0.0.0.0')
