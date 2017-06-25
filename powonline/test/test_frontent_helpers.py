756f064043994a9d1828924fe2a4fe215f873832
Michel Albert <michel@albert.lu>
2017-06-25 10:31:09 +0200

    Remove duplicate test
    This is already covered in test_core

--- powonline/test/test_frontent_helpers.py
+++ powonline/test/test_frontent_helpers.py
@@ -40,30 +40,6 @@ class TestFrontendHelpers(TestCase):
         model.DB.session.remove()
         model.DB.drop_all()
 
-    def test_fetch_assignments_core(self):
-
-        result = core.get_assignments(model.DB.session)
-        assignments_route_team_r = result['teams']['route-red']
-        assignments_route_team_b = result['teams']['route-blue']
-        assignments_route_station_r = result['stations']['route-red']
-        assignments_route_station_b = result['stations']['route-blue']
-
-        expected_assignments_route_team_r = {'team-red'}
-        expected_assignments_route_team_b = {'team-blue'}
-        expected_assignments_route_station_r = {
-            'station-start', 'station-red', 'station-end'}
-        expected_assignments_route_station_b = {
-            'station-start', 'station-blue', 'station-end'}
-
-        self.assertCountEqual(assignments_route_team_r,
-                              expected_assignments_route_team_r)
-        self.assertCountEqual(assignments_route_team_b,
-                              expected_assignments_route_team_b)
-        self.assertCountEqual(assignments_route_station_r,
-                              expected_assignments_route_station_r)
-        self.assertCountEqual(assignments_route_station_b,
-                              expected_assignments_route_station_b)
-
     def test_fetch_assignments_api(self):
         result = self.app.get('/assignments')
         result_data = json.loads(result.data.decode(result.charset))
