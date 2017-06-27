235e7392847d57129e9ab6d20baaa386b55f72e0
Michel Albert <michel@albert.lu>
2017-06-27 18:30:40 +0200

    Removed vuetify boilerplate

--- frontend/powonline/src/App.vue
+++ frontend/powonline/src/App.vue
@@ -1,32 +1,4 @@
 <template>
-  <v-app id="app" top-toolbar left-fixed-sidebar>
-    <v-toolbar>
-      <v-toolbar-side-icon @click.native.stop="sidebar = !sidebar" />
-      <v-toolbar-logo>Toolbar</v-toolbar-logo>
-    </v-toolbar>
-    <main>
-      <v-sidebar left fixed drawer v-model="sidebar">
-        <v-list>
-          <v-list-item v-for="i in 3" :key="i">
-            <v-list-tile>
-              <v-list-tile-title>Item {{ i }}</v-list-tile-title>
-            </v-list-tile>
-          </v-list-item>
-        </v-list>
-      </v-sidebar>
-      <v-content>
-        <v-container fluid>
-          <div class="title">
-            <h2>Main content</h2>
-            <v-btn primary>Primary button</v-btn>
-            <v-btn secondary>Secondary button</v-btn>
-            <v-btn success>Success button</v-btn>
-          </div>
-        </v-container>
-      </v-content>
-    </main>
-  </v-app>
-  <!--
   <div id="app">
     <router-link class="navlink" to="/">Index</router-link>
     <router-link class="navlink" to="/station">Stations</router-link>
@@ -37,7 +9,6 @@
       <error-block :error="error" v-for="(error, idx) in errors" :key="'error-' + idx"></error-block>
     </div>
   </div>
-  -->
 </template>
 
 <script>
