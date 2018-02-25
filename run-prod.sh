4a760c0c8016d02e34370a685bcfb1421418d6ca
Michel Albert <michel@albert.lu>
2018-02-25 17:07:19 +0100

    Fix startup script

--- run-prod.sh
+++ run-prod.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 docker run \
     -d \
-    -P \
+    --expose \
     --volume powonline-api-config:/etc/mamerwiselen/powonline \
     --env DSN=postgresql://powonline@powonline-db/powonline \
-    --env VIRTUAL_HOST=api.powonline.albert.lu \
+    --env VIRTUAL_HOST=powonline-api.albert.lu \
     --env VIRTUAL_PROTO=uwsgi \
-    --env LETSENCRYPT_HOST=api.powonline.albert.lu \
+    --env LETSENCRYPT_HOST=powonline-api.albert.lu \
     --env LETSENCRYPT_EMAIL=michel@albert.lu \
     exhuma/powonline-api:latest
