3cdee2fb53f4c723c1f99f3596cc3383515babd3
Michel Albert <michel@albert.lu>
2018-02-26 12:45:57 +0100

    Add route color to DB

---
+++ database/alembic/versions/f5fdb20dd1dd_route_color.py
@@ -0,0 +1,24 @@
+"""route-color
+
+Revision ID: f5fdb20dd1dd
+Revises: 157ac81c287f
+Create Date: 2018-02-26 12:44:11.341006
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'f5fdb20dd1dd'
+down_revision = '157ac81c287f'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.add_column('route', sa.Column('color', sa.Unicode()))
+
+
+def downgrade():
+    op.drop_column('route', 'color')
