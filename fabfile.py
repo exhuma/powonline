60806f2da3424f001b98839c55b913d55af272dd
Michel Albert <michel@albert.lu>
2022-03-29 07:04:23 +0200

    Add an invoke task to run the process

--- fabfile.py
+++ fabfile.py
@@ -9,3 +9,11 @@ def develop(context):
     context.run("[ -d env ] || python3 -m venv env", replace_env=False, pty=True)
     context.run("./env/bin/pip install -U pip", replace_env=False, pty=True)
     context.run("./env/bin/pip install -e .[dev,test]", replace_env=False, pty=True)
+
+
+@task
+def run(context):
+    """
+    Run a development server
+    """
+    context.run("./env/bin/python autoapp.py", replace_env=False, pty=True)
