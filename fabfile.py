986d7685dbc4eb65cdbbca066fc2d4b4cae20478
Michel Albert <michel@albert.lu>
2022-03-29 07:04:05 +0200

    Run invoke tasks in a pseudo-terminal

--- fabfile.py
+++ fabfile.py
@@ -6,6 +6,6 @@ def develop(context):
     """
     Set up a development environment
     """
-    context.run("[ -f env ] || python3 -m venv env", replace_env=False)
-    context.run("./env/bin/pip install -U pip", replace_env=False)
-    context.run("./env/bin/pip install -e .[dev,test]", replace_env=False)
+    context.run("[ -d env ] || python3 -m venv env", replace_env=False, pty=True)
+    context.run("./env/bin/pip install -U pip", replace_env=False, pty=True)
+    context.run("./env/bin/pip install -e .[dev,test]", replace_env=False, pty=True)
