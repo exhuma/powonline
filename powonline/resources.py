0b8e3468cb27c61b9fdecb4edd17fdaafa1159c6
Michel Albert <michel@albert.lu>
2023-03-24 10:41:25 +0100

    Apply black

--- powonline/resources.py
+++ powonline/resources.py
@@ -147,7 +147,9 @@ class UserList(Resource):
 
         parsed_output, errors = USER_LIST_SCHEMA.dumps(output)
         if errors:
-            LOG.critical("Unable to process return value: %r", errors, exc_info=True)
+            LOG.critical(
+                "Unable to process return value: %r", errors, exc_info=True
+            )
             return "Server was unable to process the response", 500
 
         output = make_response(parsed_output, 200)
@@ -174,7 +176,9 @@ class User(Resource):
     def _single_response(output, status_code=200):
         parsed_output, errors = USER_SCHEMA_SAFE.dumps(output)
         if errors:
-            LOG.critical("Unable to process return value: %r", errors, exc_info=True)
+            LOG.critical(
+                "Unable to process return value: %r", errors, exc_info=True
+            )
             return "Server was unable to process the response", 500
 
         response = make_response(parsed_output)
@@ -229,7 +233,9 @@ class TeamList(Resource):
 
         parsed_output, errors = TEAM_LIST_SCHEMA.dumps(output)
         if errors:
-            LOG.critical("Unable to process return value: %r", errors, exc_info=True)
+            LOG.critical(
+                "Unable to process return value: %r", errors, exc_info=True
+            )
             return "Server was unable to process the response", 500
 
         output = make_response(parsed_output, 200)
@@ -256,7 +262,9 @@ class Team(Resource):
     def _single_response(output, status_code=200):
         parsed_output, errors = TEAM_SCHEMA.dumps(output)
         if errors:
-            LOG.critical("Unable to process return value: %r", errors, exc_info=True)
+            LOG.critical(
+                "Unable to process return value: %r", errors, exc_info=True
+            )
             return "Server was unable to process the response", 500
 
         response = make_response(parsed_output)
@@ -315,7 +323,9 @@ class StationList(Resource):
 
         parsed_output, errors = STATION_LIST_SCHEMA.dumps(output)
         if errors:
-            LOG.critical("Unable to process return value: %r", errors, exc_info=True)
+            LOG.critical(
+                "Unable to process return value: %r", errors, exc_info=True
+            )
             return "Server was unable to process the response", 500
 
         output = make_response(parsed_output, 200)
@@ -342,7 +352,9 @@ class Station(Resource):
     def _single_response(output, status_code=200):
         parsed_output, errors = STATION_SCHEMA.dumps(output)
         if errors:
-            LOG.critical("Unable to process return value: %r", errors, exc_info=True)
+            LOG.critical(
+                "Unable to process return value: %r", errors, exc_info=True
+            )
             return "Server was unable to process the response", 500
 
         response = make_response(parsed_output)
@@ -384,7 +396,9 @@ class RouteList(Resource):
 
         parsed_output, errors = ROUTE_LIST_SCHEMA.dumps(output)
         if errors:
-            LOG.critical("Unable to process return value: %r", errors, exc_info=True)
+            LOG.critical(
+                "Unable to process return value: %r", errors, exc_info=True
+            )
             return "Server was unable to process the response", 500
 
         output = make_response(parsed_output, 200)
@@ -411,7 +425,9 @@ class Route(Resource):
     def _single_response(output, status_code=200):
         parsed_output, errors = ROUTE_SCHEMA.dumps(output)
         if errors:
-            LOG.critical("Unable to process return value: %r", errors, exc_info=True)
+            LOG.critical(
+                "Unable to process return value: %r", errors, exc_info=True
+            )
             return "Server was unable to process the response", 500
 
         response = make_response(parsed_output)
@@ -651,7 +667,9 @@ class TeamStation(Resource):
 
             parsed_output, errors = STATION_LIST_SCHEMA.dumps(output)
             if errors:
-                LOG.critical("Unable to process return value: %r", errors, exc_info=True)
+                LOG.critical(
+                    "Unable to process return value: %r", errors, exc_info=True
+                )
                 return "Server was unable to process the response", 500
 
             output = make_response(parsed_output, 200)
