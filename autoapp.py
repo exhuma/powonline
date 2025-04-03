55269bd4315a67613354f1b07906ca0bf483af20
Michel Albert <michel@albert.lu>
2025-04-03 07:07:34 +0200

    src-folder layout

--- autoapp.py
+++ autoapp.py
@@ -16,15 +16,15 @@ config = default()
 colorize_werkzeug()
 APP = make_app(config)
 
-if not exists("cert.cert"):
+if not exists("ssl/cert.cert"):
     print("You don't seem to have a SSL cert available for the dev server")
     print("The following commands should allow you to create one")
     print(
         """
-    openssl req -new > new.ssl.csr
-    openssl rsa -in privkey.pem -out key.key
-    openssl x509 -in new.ssl.csr -out cert.cert -req -signkey key.key -days 365
-    """
+        openssl req -new > ssl/new.ssl.csr
+        openssl rsa -in ssl/privkey.pem -out ssl/key.key
+        openssl x509 -in new.ssl.csr -out ssl/cert.cert -req -signkey ssl/key.key -days 365
+        """
     )
     print(80 * "=")
     print("Running without SSL")
@@ -32,8 +32,8 @@ if not exists("cert.cert"):
     SSL = None
 else:
     print(80 * "=")
-    print("Using cert.cert and key.keyas SSL context")
+    print("Using ssl/cert.cert and ssl/key.key as SSL context")
     print(80 * "=")
-    SSL = ("cert.cert", "key.key")
+    SSL = ("ssl/cert.cert", "ssl/key.key")
 
 APP.run(debug=True, host="0.0.0.0", ssl_context=SSL)
