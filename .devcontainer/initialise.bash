5ca5db7992edaca0241312e78d1b5bc9d86c5dc6
Michel Albert <michel@albert.lu>
2025-04-06 10:16:17 +0200

    Update devcontainer init

--- .devcontainer/initialise.bash
+++ .devcontainer/initialise.bash
@@ -9,14 +9,17 @@ set -xe
 
 suto apt-get update && sudo apt-get install -y entr
 
-pipx install alembic
-pipx inject alembic psycopg2-binary
-pipx install fabric
-pipx inject fabric decorator  # <- fabric bugfix
-pipx install pre-commit
+curl -LsSf https://astral.sh/uv/install.sh | sh
+
+uv tool install --with psycopg2-binary alembic
+uv tool install fabric
+uv tool install pre-commit
+
 fab develop
 
 (cd database && alembic upgrade head)
 (cd database && alembic show head)
 
-psql -v ON_ERROR_STOP=1 -X1qf .devcontainer/sample-data.sql postgresql://postgres:postgres@db/powonline
+psql -v ON_ERROR_STOP=1 -X1qf \
+    .devcontainer/sample-data.sql \
+    postgresql://postgres:postgres@db/powonline
