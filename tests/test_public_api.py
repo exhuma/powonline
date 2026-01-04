1422dea3d868a60461628c0d34d262ebd5a735cf
Michel Albert <michel@albert.lu>
2026-01-04 13:35:05 +0100

    linting

--- tests/test_public_api.py
+++ tests/test_public_api.py
@@ -9,7 +9,6 @@ import pytest
 from config_resolver.core import get_config
 from fastapi import FastAPI
 from httpx import AsyncClient
-from powonline.auth import User, get_user
 from sqlalchemy import text
 from sqlalchemy.ext.asyncio import AsyncSession
 from util import (
@@ -20,6 +19,7 @@ from util import (
 )
 
 from powonline import schema
+from powonline.auth import User, get_user
 
 LOG = logging.getLogger(__name__)
 
